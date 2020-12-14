636a361fb3f201ceb099dd7d120914acee751355
Moustafa Mohamed <darsh.m7md@gmail.com>
2020-12-14 18:17:39 +0200

    Create AlertBox Reformatting Main class Solving Model 1

--- src/myqueue/Main.java
+++ src/myqueue/Main.java
@@ -30,13 +30,15 @@ public class Main extends Application {
     private Label modelLabel, lambdaLabel, muLabel, capacityKLAbel, capacityK_minus1_label, serversCLabel, initialNumberMLabel;
     private TextField lambdaInput, muInput, capacityKInput, capacityK_minus1_input, initialNumberMInput, ServersCInput;
     private Button queryButton, graphButton, clearButton;
+    private HBox hb;
+    private ComboBox<String> modelBox;
     
     @Override
     public void start(Stage primaryStage) {
         homeWindow = primaryStage;
         homeWindow.setMinWidth(330);
         homeWindow.setMinHeight(350);
-        homeWindow.setOnCloseRequest(e -> System.out.println("width: " + homeWindow.getWidth() + ", Height: " + homeWindow.getHeight()));
+//        homeWindow.setOnCloseRequest(e -> System.out.println("width: " + homeWindow.getWidth() + ", Height: " + homeWindow.getHeight()));
         
         homeLayout = new GridPane();
         
@@ -64,31 +66,20 @@ public class Main extends Application {
         initialNumberMInput = new TextField();
         initialNumberMInput.setMaxWidth(200);
         
-        ToolTips:
-            lambdaLabel.setTooltip(new Tooltip("Inter-arrival rate"));
-            lambdaInput.setTooltip(new Tooltip("Inter-arrival rate"));
-            muLabel.setTooltip(new Tooltip("service rate"));
-            muInput.setTooltip(new Tooltip("service rate"));
-            capacityKLAbel.setTooltip(new Tooltip("capacity of the system"));
-            capacityKInput.setTooltip(new Tooltip("capacity of the system"));
-            capacityK_minus1_label.setTooltip(new Tooltip("capacity of the system"));
-            capacityK_minus1_input.setTooltip(new Tooltip("capacity of the system"));
-            serversCLabel.setTooltip(new Tooltip("Number of parallel servers"));
-            ServersCInput.setTooltip(new Tooltip("Number of parallel servers"));
-            initialNumberMInput.setTooltip(new Tooltip("Initial Number of customers"));
-            initialNumberMLabel.setTooltip(new Tooltip("Initial Number of customers"));
-        
         queryButton = new Button("Query");
         graphButton = new Button("Graph");
         clearButton = new Button("Clear");
+                    
+        setToolTips();
         
-        clearButton.setOnAction(e -> clear());
         
         initModel(1);
         
+        clearButton.setOnAction(e -> clear());
+        
         modelLabel = new Label("Model: ");
         
-        ComboBox<String> modelBox = new ComboBox<>();
+        modelBox = new ComboBox<>();
         modelBox.getItems().add("D/D/1/K-1");
         modelBox.getItems().add("M/M/1");
         modelBox.getItems().add("M/M/1/K");
@@ -97,41 +88,51 @@ public class Main extends Application {
         modelBox.getSelectionModel().selectFirst();
         
         modelBox.setOnAction(e -> {
-            if(modelBox.getSelectionModel().getSelectedIndex() + 1 == 1) queryButton.setText("Query");
-            else queryButton.setText("Solve");
-            initModel(modelBox.getSelectionModel().getSelectedIndex() + 1);
+            int selected_index = modelBox.getSelectionModel().getSelectedIndex();
+            if(selected_index == 0){
+                queryButton.setText("Query");
+                graphButton.setDisable(false);
+            }
+            else {
+                queryButton.setText("Solve");
+                graphButton.setDisable(true);
+            }
+            initModel(selected_index + 1);
         });
 
+        queryButton.setOnAction(e -> {
+            int selected_model = modelBox.getSelectionModel().getSelectedIndex() + 1;
+            
+            if(selected_model == 1){
+                queryModel1();
+            }
+        });
+        
+        
         homeLayout.setPadding(new Insets(8, 8, 8, 8));
         homeLayout.setHgap(10);
         homeLayout.setVgap(10);
         homeLayout.getChildren().add(modelBox);
         
-        HBox hb = new HBox(queryButton, graphButton, clearButton);
+        hb = new HBox(queryButton, graphButton, clearButton);
         hb.setPadding(new Insets(8, 8, 8, 8));
         hb.setSpacing(20);
         
-        Constraints:
-            GridPane.setConstraints(modelLabel, 0, 0);
-            GridPane.setConstraints(modelBox, 1, 0);
-            GridPane.setConstraints(lambdaLabel, 0, 1);
-            GridPane.setConstraints(lambdaInput, 1, 1);
-            GridPane.setConstraints(muLabel, 0, 2);
-            GridPane.setConstraints(muInput, 1, 2);
-            GridPane.setConstraints(capacityK_minus1_label, 0, 3);
-            GridPane.setConstraints(capacityK_minus1_input, 1, 3);
-            GridPane.setConstraints(initialNumberMLabel, 0, 4);
-            GridPane.setConstraints(initialNumberMInput, 1, 4);
-            GridPane.setConstraints(capacityKLAbel, 0, 5);
-            GridPane.setConstraints(capacityKInput, 1, 5);
-            GridPane.setConstraints(serversCLabel, 0, 6);
-            GridPane.setConstraints(ServersCInput, 1, 6);
-            GridPane.setConstraints(hb, 1, 7);
+        setConstraints();
         
         
         
-        homeLayout.getChildren().addAll(initialNumberMLabel,initialNumberMInput, modelLabel, lambdaLabel, muLabel, capacityKLAbel, capacityK_minus1_label, serversCLabel, hb
-        ,lambdaInput, muInput, capacityKInput, capacityK_minus1_input, ServersCInput);
+        homeLayout.getChildren().addAll(
+         initialNumberMLabel,
+         initialNumberMInput, 
+         modelLabel, lambdaLabel,
+         muLabel, capacityKLAbel,
+         capacityK_minus1_label,
+         serversCLabel, hb,
+         lambdaInput, muInput,
+         capacityKInput, capacityK_minus1_input,
+         ServersCInput
+        );
         
         Scene scene = new Scene(homeLayout);
         
@@ -143,19 +144,18 @@ public class Main extends Application {
     private void initModel(int n){
         switch (n) {
             case 1:
+                // D/D/1/K-1
                 if(last_model_selected == 1) return;
                 setDisableAll(true);
-                
                 initialNumberMInput.setDisable(false);
                 initialNumberMInput.setText("0");
+                capacityK_minus1_input.setDisable(false);
                 last_model_selected = 1;
-                
                 break;
             case 2:
                 // M/M/1
                 if(last_model_selected == 2) return;
                 setDisableAll(true);
-                
                 last_model_selected = 2;
                 break;
             case 3:
@@ -200,11 +200,116 @@ public class Main extends Application {
         initialNumberMInput.setDisable(status);
         ServersCInput.setDisable(status);
     }
+    
+    private void queryModel1(){
+        double lambda, mu;
+        int k_minus_1, initial_number_M;
+        if(!checkLambdaAndMu()) return;
+        
+        lambda = Double.parseDouble(lambdaInput.getText());
+         mu = Double.parseDouble(muInput.getText());
+        
+        try{
+            if(capacityK_minus1_input.getText().trim().length() == 0)
+                throw new NumberFormatException();
+            k_minus_1 = Integer.parseInt(capacityK_minus1_input.getText());
+            
+            if(k_minus_1 < 0)
+                throw new NumberFormatException();
+            
+        }catch(NumberFormatException e){
+            ErrorBox.display("Error!", "You must enter a non-negative integer number for K-1");
+            return;
+        }
+        
+        try{
+            if(initialNumberMInput.getText().trim().length() == 0)
+                initial_number_M = 0;
+            else 
+                initial_number_M = Integer.parseInt(initialNumberMInput.getText());
+            
+            if(initial_number_M < 0)
+                throw  new NumberFormatException();
+            
+        }catch(NumberFormatException e){
+            ErrorBox.display("Error!", "You must enter a non-negative integer number for M");
+            return;
+        }
+        
+        if(lambda > mu && initial_number_M != 0){
+            AlertBox.display("Alert", "M is ignored becuase λ is greater than μ");
+            initial_number_M = 0;
+        }
+        
+        Model1.solve(lambda, mu, k_minus_1, initial_number_M);
+
+    }
+    
+    private boolean checkLambdaAndMu(){
+        try{
+            if(lambdaInput.getText().trim().length() == 0)
+                throw new NumberFormatException();
+            if(Double.parseDouble(lambdaInput.getText().trim()) <= 0)
+                throw new NumberFormatException();
+        }catch(NumberFormatException e){
+            ErrorBox.display("Error!", "You must enter a positive real number for λ");
+            return false;
+        }
+        
+        try{
+            if(muInput.getText().trim().length() == 0)
+                throw new NumberFormatException();
+            if(Double.parseDouble(muInput.getText().trim()) <= 0)
+                throw new NumberFormatException();
+        }catch(NumberFormatException e){
+            ErrorBox.display("Error!", "You must enter a positive real number for μ");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    
+    private void setToolTips(){
+        lambdaLabel.setTooltip(new Tooltip("Inter-arrival rate"));
+        lambdaInput.setTooltip(new Tooltip("Inter-arrival rate"));
+        muLabel.setTooltip(new Tooltip("service rate"));
+        muInput.setTooltip(new Tooltip("service rate"));
+        capacityKLAbel.setTooltip(new Tooltip("capacity of the system"));
+        capacityKInput.setTooltip(new Tooltip("capacity of the system"));
+        capacityK_minus1_label.setTooltip(new Tooltip("capacity of the system"));
+        capacityK_minus1_input.setTooltip(new Tooltip("capacity of the system"));
+        serversCLabel.setTooltip(new Tooltip("Number of parallel servers"));
+        ServersCInput.setTooltip(new Tooltip("Number of parallel servers"));
+        initialNumberMInput.setTooltip(new Tooltip("Initial Number of customers"));
+        initialNumberMLabel.setTooltip(new Tooltip("Initial Number of customers"));
+        clearButton.setTooltip(new Tooltip("Clear inputs"));
+    }
+    
+    private void setConstraints() {
+        GridPane.setConstraints(modelLabel, 0, 0);
+        GridPane.setConstraints(modelBox, 1, 0);
+        GridPane.setConstraints(lambdaLabel, 0, 1);
+        GridPane.setConstraints(lambdaInput, 1, 1);
+        GridPane.setConstraints(muLabel, 0, 2);
+        GridPane.setConstraints(muInput, 1, 2);
+        GridPane.setConstraints(capacityK_minus1_label, 0, 3);
+        GridPane.setConstraints(capacityK_minus1_input, 1, 3);
+        GridPane.setConstraints(initialNumberMLabel, 0, 4);
+        GridPane.setConstraints(initialNumberMInput, 1, 4);
+        GridPane.setConstraints(capacityKLAbel, 0, 5);
+        GridPane.setConstraints(capacityKInput, 1, 5);
+        GridPane.setConstraints(serversCLabel, 0, 6);
+        GridPane.setConstraints(ServersCInput, 1, 6);
+        GridPane.setConstraints(hb, 1, 7);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
+
     
 }
